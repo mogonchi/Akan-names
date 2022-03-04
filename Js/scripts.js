14a84cbcd834a49c048b1da7a5e7d11633416e9d
mogonchi <monyenyedolvine@gmail.com>
2022-03-04 16:57:45 +0300

    add changes

--- Js/scripts.js
+++ Js/scripts.js
@@ -1,6 +1,6 @@
 var akan = function(yy, mm, dd, g) {
     var femaleNames = ["Akosua", "Adwao", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-    var malenames = ["Kwasi", "Kudwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    var maleNames = ["Kwasi", "Kudwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
     var dd = newDay(yy, mm, dd);
     if (g === "Female") {
         return dd && femaleNames[dd.getDay()];
