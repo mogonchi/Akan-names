5b9b9ed85507b5079ae7f3ca8d8fab48288dec11
mogonchi <monyenyedolvine@gmail.com>
2022-03-04 18:54:34 +0300

    add changes to index.html

--- Js/scripts.js
+++ Js/scripts.js
@@ -1,19 +1,28 @@
-var akan = function(yy, mm, dd, g) {
+var akan = function(cc, yy, mm, dd, g) {
     var femaleNames = ["Akosua", "Adwao", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
     var maleNames = ["Kwasi", "Kudwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-    var dd = newDay(yy, mm, dd);
+    var day = ( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd )%7
+    day = Math.trunc(day)
     if (g === "Female") {
-        return dd && femaleNames[dd.getDay()];
+        alert(femaleNames[day]); 
     } else {
-        return dd && maleNames[dd.getDay()];
+        alert(maleNames[day]);
     }
 }
-var year = document.getElementById("year");
-var month = document.getElementById("month");
-var day = document.getElementById("day");
+var year = document.getElementById("year").value;
+var month = document.getElementById("month").value;
+var day = document.getElementById("day").value;
 var gender = document.querySelector('input[name="gender"]:checked');
 var button = document.getElementById("submit");
+var century = year.substring(0, 2);
+var partYear = year.substring(2, 4);
 
-button.addEventListener("click", function(){
-    alert("name")
+button.addEventListener("click", function(){ 
+    console.log(gender)
+    century = parseInt(century);
+    month = parseInt(month);
+    partYear = parseInt(partYear);
+    day = parseInt(day);
+    
+    akan(century, partYear, month, day, gender.value);
 })
